c2069ed729b6cd76bcf61939d8e21f1220e65c96
alexey <kalexey@concerteza.ru>
2011-05-12 11:22:57 +0000

    minor utils additions (no ticket)

--- src/main/java/ru/concerteza/util/version/Version.java
+++ src/main/java/ru/concerteza/util/version/Version.java
@@ -2,18 +2,27 @@ package ru.concerteza.util.version;
 
 import org.apache.commons.lang.builder.ToStringBuilder;
 import org.apache.commons.lang.builder.ToStringStyle;
+import ru.concerteza.util.namedregex.NamedMatcher;
+import ru.concerteza.util.namedregex.NamedPattern;
+
+import static java.lang.Integer.parseInt;
 
 /**
  * User: alexey
  * Date: 5/11/11
  */
 public class Version {
+    private static final NamedPattern STD_VERSION_PATTERN = NamedPattern.compile(
+            "^(?<major_version>\\d+)\\.(?<minor_version>\\d+).*$");
+
     private final String specificationTitle;
     private final String specificationVersion;
     private final String specificationVendor;
     private final String implementationTitle;
     private final String implementationVersion;
     private final String implementationVendor;
+    private final int majorVersion;
+    private final int minorVersion;
 
     public Version(String specificationTitle, String specificationVersion, String specificationVendor, String implementationTitle, String implementationVersion, String implementationVendor) {
         this.specificationTitle = specificationTitle;
@@ -22,6 +31,14 @@ public class Version {
         this.implementationTitle = implementationTitle;
         this.implementationVersion = implementationVersion;
         this.implementationVendor = implementationVendor;
+        NamedMatcher matcher = STD_VERSION_PATTERN.matcher(specificationVersion);
+        if(matcher.matches()) {
+            majorVersion = parseInt(matcher.group("major_version"));
+            minorVersion = parseInt(matcher.group("minor_version"));
+        } else {
+            majorVersion = -1;
+            minorVersion = -1;
+        }
     }
 
     public String getSpecificationTitle() {
@@ -48,9 +65,18 @@ public class Version {
         return implementationVendor;
     }
 
+    // git rev-parse --short HEAD
     public String getShortImplementationVersion() {
         if(implementationVersion.length() > 7) return implementationVersion.substring(0, 7);
         else return implementationVersion;
+    }
+
+    public int getMajorVersion() {
+        return majorVersion;
+    }
+
+    public int getMinorVersion() {
+        return minorVersion;
     }
 
     @Override
@@ -62,6 +88,8 @@ public class Version {
                 append("implementationTitle", implementationTitle).
                 append("implementationVersion", implementationVersion).
                 append("implementationVendor", implementationVendor).
+                append("majorVersion", majorVersion).
+                append("minorVersion", minorVersion).
                 toString();
     }
 }
